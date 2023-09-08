86e077e3f8e4fc8238ff9007cac5229f133fc33f
tenma <tenma@osxt.com>
2023-09-08 19:11:47 +0200

    test new build

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,14 +1,17 @@
-import './About.css'
-import banner from "../../assets/about_banner.png"
-import {Banner} from "../../components/Banner/Banner.jsx";
-import {Accordion} from "../../components/Accordion/Accordion.jsx";
-import {aboutAccordion} from "./aboutAccordion.js"
+import "./About.css";
+import banner from "../../assets/about_banner.png";
+import { Banner } from "../../components/Banner/Banner.jsx";
+import { Accordion } from "../../components/Accordion/Accordion.jsx";
+import { aboutAccordion } from "./aboutAccordion.js";
 export const About = () => {
-
-    return (<section className="about">
-        <Banner img={banner}/>
-        {aboutAccordion.map((item,key) => (
-            <Accordion key={key} title={item.title}>{item.content}</Accordion>
-        ))}
-    </section>)
-}
+  return (
+    <section className="about">
+      <Banner img={banner} />
+      {aboutAccordion.map((item, key) => (
+        <Accordion key={key} title={item.title}>
+          {item.content}
+        </Accordion>
+      ))}
+    </section>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
