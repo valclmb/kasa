dd956151840bc01e025fabc38ca9a910768b4133
tenma <tenma@osxt.com>
2023-09-08 19:30:57 +0200

    fix build

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,31 @@
-import logo from "../../assets/logo.png"
-import './Header.css'
-import {useLocation,Link} from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./Header.css";
+import { useLocation, Link } from "react-router-dom";
 export const Header = () => {
-    const nav = [{title: "Accueil", href: "/home"}, {title: "A propos", href: "/about"}]
-    const location = useLocation().pathname;
+  const nav = [
+    { title: "Accueil", href: "/home" },
+    { title: "A propos", href: "/about" },
+  ];
+  const location = useLocation().pathname;
 
-    return (<header className="header">
-        <h1><Link to="/home"><img src={logo} alt="Kasa"/></Link></h1>
-        <nav>
-            {nav.map(({title, href}) => (
-            <a key={href} href={href} className={location === href ? "location--active" : ""}>{title}</a>
+  return (
+    <header className="header">
+      <h1>
+        <Link to="/home">
+          <img src={logo} alt="Kasa" />
+        </Link>
+      </h1>
+      <nav>
+        {nav.map(({ title, href }) => (
+          <Link
+            key={href}
+            to={href}
+            className={location === href ? "location--active" : ""}
+          >
+            {title}
+          </Link>
         ))}
-        </nav>
-    </header>)
-}
+      </nav>
+    </header>
+  );
+};
