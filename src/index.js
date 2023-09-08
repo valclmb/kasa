b6bb54d2eb9dbe21e47e154eeafb9c567ae50474
tenma <tenma@osxt.com>
2023-09-08 19:01:36 +0200

    test hashrouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter router={router} />
   </React.StrictMode>
 );
